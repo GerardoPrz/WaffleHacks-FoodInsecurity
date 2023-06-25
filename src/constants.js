eafa55f3d538423555cdf7219289ced75efbae1b
yeicobF <jacobrdz34@gmail.com>
2023-06-25 07:56:29 -0600

    Add food times constant

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const FOOD_TIMES = {
+  breakfast: {
+    order: 0,
+    name: "breakfast",
+    title: "Desayuno",
+  },
+  lunch: {
+    order: 1,
+    name: "lunch",
+    title: "Comida",
+  },
+  dinner: {
+    order: 2,
+    name: "dinner",
+    title: "Cena",
+  },
+}
