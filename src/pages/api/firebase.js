d66138449529588d70860e97434e68e1f5f24c6f
GerardoPrz <60196783+GerardoPrz@users.noreply.github.com>
2023-06-24 21:50:46 -0600

    Update firebase.js

--- src/pages/api/firebase.js
+++ src/pages/api/firebase.js
@@ -7,11 +7,12 @@ import {
   signOut,
 } from "firebase/auth";
 
-import {
-    getFirestore,
-    doc,
-    setDoc,
-} from "firebase/firestore";
+import { 
+    getFirestore, 
+    doc, 
+    setDoc, 
+    getDocs,
+    collection } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC2ps1m1koNXDLcElKtg0LN-bl3KvWNlLo",
@@ -33,7 +34,6 @@ export const login = async (email, password) => {
   try {
     //email and password
     const result = await signInWithEmailAndPassword(auth, email, password);
-    console.log(result);
   } catch (error) {
     throw new Error(error);
   }
@@ -42,8 +42,12 @@ export const login = async (email, password) => {
 export const register = async (email, password) => {
   try {
     // Registrar usuario con email y contraseña
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
     // Crear un documento para el usuario en Firestore
     const userDocRef = doc(db, "users", userCredential.user.uid);
     const userData = {
@@ -72,6 +76,83 @@ export const resetPassword = async (email) => {
 export const logout = async () => {
   try {
     await signOut(auth);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const addRecipes = async (recipe) => {
+  try {
+    // Crear la receta en la colección "recipes"
+    const recipeRef = await addDoc(collection(db, "recipes"), recipe);
+    const recipeId = recipeRef.id;
+
+    // Obtener el ID del usuario actual
+    const userId = auth.currentUser.uid;
+
+    // Actualizar el array de recetas del usuario con el nuevo ID de la receta
+    const userDocRef = doc(db, "users", userId);
+    await updateDoc(userDocRef, {
+      recipes: arrayUnion(recipeId),
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const addMenu = async (menu) => {
+  try {
+    // Crear el menú en la colección "menus"
+    const menuRef = await addDoc(collection(db, "menus"), menu);
+    const menuId = menuRef.id;
+
+    // Obtener el ID del usuario actual
+    const userId = auth.currentUser.uid;
+
+    // Actualizar el array de menús del usuario con el nuevo ID del menú
+    const userDocRef = doc(db, "users", userId);
+    await updateDoc(userDocRef, {
+      menus: arrayUnion(menuId),
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const addFavoriteRecipe = async (recipeId) => {
+  try {
+    // Obtener el ID del usuario actual
+    const userId = auth.currentUser.uid;
+
+    // Actualizar el array de recetas favoritas del usuario con el nuevo ID de la receta
+    const userDocRef = doc(db, "users", userId);
+    await updateDoc(userDocRef, {
+      favoriteRecipes: arrayUnion(recipeId),
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const addFavoriteMenu = async (menuId) => {
+  try {
+    // Obtener el ID del usuario actual
+    const userId = auth.currentUser.uid;
+
+    // Actualizar el array de recetas favoritas del usuario con el nuevo ID de la receta
+    const userDocRef = doc(db, "users", userId);
+    await updateDoc(userDocRef, {
+      favoriteMenus: arrayUnion(menuId),
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getFood = async () => {
+  try {
+    const food = await getDocs(collection(db, "food"));
+    return food.docs.map((doc) => doc.data());
   } catch (error) {
     throw new Error(error);
   }
