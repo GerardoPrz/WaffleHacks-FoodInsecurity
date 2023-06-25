2d46e12c990f41af9676ede4afea573a5fa49d7e
yeicobF <jacobrdz34@gmail.com>
2023-06-24 21:10:32 -0600

    Config(front): Add tailwind color and center containers

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       colors: {
         primary: "#28BC63",
         secondary: "#8F96A0",
         text: "#3F3D56",
         light: "#FDFDFD",
+        dark: "#1C1B1F",
         disabled: "#F2F5F9",
       },
     },
