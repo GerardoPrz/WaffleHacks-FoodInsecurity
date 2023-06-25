6cc53bc8e092da9c8361d350a96534fa537ecd80
yeicobF <jacobrdz34@gmail.com>
2023-06-24 21:13:02 -0600

    Feat(front): Add app layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,10 @@
 import "@/styles/globals.css"
-import Head from "next/head"
+import { Layout } from "@/components/Layout"
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Nourish</title>
-        <meta
-          name="description"
-          content="Prevent food insecurity in students by allowing them to easily find food resources on campus and their home."
-        />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <div className="bg-gray-800">
-        <Component {...pageProps} />
-      </div>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
