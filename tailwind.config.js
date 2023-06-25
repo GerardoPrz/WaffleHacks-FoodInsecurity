4d737c34cd55f4659f7066cd67fe1f3f845c8888
yeicobF <jacobrdz34@gmail.com>
2023-06-24 20:12:44 -0600

    front: Add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#28BC63",
+        secondary: "#8F96A0",
+        text: "#3F3D56",
+        light: "#FDFDFD",
+        disabled: "#F2F5F9",
+      },
+    },
   },
   plugins: [],
 }
