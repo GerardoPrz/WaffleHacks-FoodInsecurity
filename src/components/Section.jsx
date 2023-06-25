4da2953dbd3af27413d2cca98d3dd26196b07bb4
yeicobF <jacobrdz34@gmail.com>
2023-06-25 02:40:07 -0600

    Feat(front): Add section components

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,3 +1,11 @@
+export function Section({ children }) {
+  return <section className="flex flex-col gap-2">{children}</section>
+}
+
 export function SectionHeader({ children }) {
   return <header className="flex flex-col gap-2">{children}</header>
+}
+
+export function SectionTitle({ children }) {
+  return <h2 className="text-lg">{children}</h2>
 }
