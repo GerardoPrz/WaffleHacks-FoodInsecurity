9e83abbf91e62665afdb2a8b06edbf3f18e621fd
yeicob <jacobrdz34@gmail.com>
2023-09-24 20:37:57 -0600

    fix: add optional chaining to food card key

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -5,7 +5,7 @@ export function FoodIconsList({ foodList }) {
     <div className="grid gap-2 grid-cols-3 w-full">
       {foodList?.map((food) => (
         <FoodCard
-          key={food.id}
+          key={food?.id}
           food={food}
           isSelected={false}
           size="sm"
